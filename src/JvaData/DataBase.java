7826d71317c3e0fab6126515290e6989b85d0963
Gabriela Dumitru <dumitrugabriela250@gmail.com>
2025-04-12 23:57:29 +0300

    adding data base connection

---
+++ src/JvaData/DataBase.java
@@ -0,0 +1,8 @@
+package JvaData;
+
+public class DataBase {
+    public static void main(String[] args) {
+        System.out.println("Connect to database");
+        System.out.println("");
+    }
+}
