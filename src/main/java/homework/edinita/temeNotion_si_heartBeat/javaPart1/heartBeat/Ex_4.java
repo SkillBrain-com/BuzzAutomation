a8aadb7e8b00d2fcd0d08b482bd2da7fef998a2f
NITA EDUARD <nitaedi@Nitas-MacBook-Air.local>
2025-05-06 21:04:40 +0300

    javaBasicPart1

---
+++ src/main/java/homework/edinita/temeNotion_si_heartBeat/javaPart1/heartBeat/Ex_4.java
@@ -0,0 +1,9 @@
+package homework.edinita.temeNotion_si_heartBeat.javaPart1.heartBeat;
+
+public class Ex_4 {
+    public static void main(String[] args) {
+        float a = 12;
+
+        System.out.println((int) a);
+    }
+}
