d70d0946585419ecb17c0e1cbc220f64e22d65eb
Burhala Andrei <iandrei59@yahoo.com>
2025-07-15 19:02:31 +0300

    Java 3 Homework

---
+++ src/main/java/homework/AndreiB/Homework/homework1/Tema8.java
@@ -0,0 +1,16 @@
+package homework.AndreiB.Homework.homework1;
+
+public class Tema8 {
+
+        public static void main(String[] args) {
+            int x = 2;
+            int y = 12;
+            // y = x * y;
+            y = x + y;
+            x = y - x;
+            y = y - x;
+            System.out.println(x); // 12
+            System.out.println(y); // 2
+        }
+
+}
---
+++ src/main/java/homework/AndreiB/Homework/homework2/Tema8.java
@@ -0,0 +1,15 @@
+package homework.AndreiB.Homework.homework2;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Tema8 {
+
+        public static void main(String[] args) {
+            int[] array = {};
+            List<Integer> lista = new ArrayList<>();
+
+            System.out.println("Array gol? " + (array.length == 0));
+            System.out.println("Lista goală? " + lista.isEmpty());
+        }
+
+}
